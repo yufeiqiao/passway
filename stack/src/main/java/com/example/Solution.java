a38b55796f5152e23ff271de86351b73bd43a807
Yufei Qiao <yufeiqiao@outlook.com>
2026-03-10 18:19:18 +0800

    lc 735, collision, done, recursively

--- stack/src/main/java/com/example/Solution.java
+++ stack/src/main/java/com/example/Solution.java
@@ -1,12 +1,15 @@
 package com.example;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
+import java.util.Iterator;
+import java.util.List;
 
-// leetcode 2390, stack
 public class Solution {
-    public String removeStars(String s) {
 
+    // leetcode 2390, stack
+    public String removeStars(String s) {
         Deque<Character> stack = new ArrayDeque<>();
         char[] src = s.toCharArray();
 
@@ -24,5 +27,39 @@ public class Solution {
         }
 
         return res.reverse().toString();
+    }
+
+    // leetcode 735, stack
+    public int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int a : asteroids) {
+            stack = collide(stack, a);
+        }
+
+        int[] res = new int[stack.size()];
+        int i = 0;
+        for (Iterator<Integer> it = stack.descendingIterator(); it.hasNext();) {
+            res[i++] = it.next();
+        }
+        return res;
+    }
+
+    public Deque<Integer> collide(Deque<Integer> stack, int asteroid) {
+        if (asteroid >= 0 || stack.peek() == null || stack.peek() < 0) {
+            stack.push(asteroid);
+            return stack;
+        }
+
+        // at here : asteroid < 0
+        // stack top not null and >= 0
+        if (asteroid + stack.peek() > 0) {
+            return stack;
+        } else if (asteroid + stack.peek() == 0) {
+            stack.pop();
+            return stack;
+        } else {
+            stack.pop();
+            return collide(stack, asteroid);
+        }
     }
 }
--- stack/src/test/java/StackTest.java
+++ stack/src/test/java/StackTest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
@@ -15,12 +16,21 @@ public class StackTest {
 
     @Test
     public void removeStarsTest() {
-
         assertEquals("lecoe", sol.removeStars("leet**cod*e"));
         assertEquals("", sol.removeStars("erase*****"));
         assertEquals("lecoe", sol.removeStars("leet**cod*e"));
         assertEquals("lecoe", sol.removeStars("leet**cod*e"));
-
     }
 
+    @Test
+    public void asteroidCollisionTest() {
+        int[] a = new int[] { 5, 10, -5 };
+        assertArrayEquals(new int[] { 5, 10 }, sol.asteroidCollision(a));
+        int[] b = new int[] { 5, -5 };
+        assertArrayEquals(new int[] {}, sol.asteroidCollision(b));
+        int[] c = new int[] { 3, 5, -6, 2, -1, 4 };
+        assertArrayEquals(new int[] { -6, 2, 4 }, sol.asteroidCollision(c));
+        int[] d = new int[] { 10, 2, -5 };
+        assertArrayEquals(new int[] { 10 }, sol.asteroidCollision(d));
+    }
 }
