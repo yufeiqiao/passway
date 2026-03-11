c2627ab78a9c0ec8579ee9b3366951568a7d1520
Yufei Qiao <yufeiqiao@outlook.com>
2026-03-11 18:02:49 +0800

    lc 935, senate voting, done

--- queue/src/main/java/com/example/Solution.java
+++ queue/src/main/java/com/example/Solution.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class Solution {
 
     public int[] recentCounter(int[] pingSequence) {
@@ -14,4 +17,52 @@ public class Solution {
         return res;
     }
 
+    public String predictPartyVictory(String senate) {
+        Queue<Character> voting = new ArrayDeque<>();
+        int rCount = 0, dCount = 0;
+        int banNextR = 0, banNextD = 0;
+
+        for (int i = 0; i < senate.length(); i++) {
+            if (senate.charAt(i) == 'R') {
+                rCount++;
+            } else {
+                dCount++;
+            }
+            voting.offer(senate.charAt(i));
+        }
+
+        while (voting.peek() != null) {
+            char seat = voting.poll();
+
+            // ban check
+            if ((banNextD > 0) && (seat == 'D')) {
+                dCount--;
+                banNextD--;
+                continue;
+            } else if ((banNextR > 0) && (seat == 'R')) {
+                rCount--;
+                banNextR--;
+                continue;
+            }
+
+            // win check
+            if ((seat == 'R') && (dCount == 0)) {
+                return "Radiant";
+            } else if ((seat == 'D') && (rCount == 0)) {
+                return "Dire";
+            }
+
+            // ban next opposite side senate
+            if (seat == 'R') {
+                banNextD++;
+            } else {
+                banNextR++;
+            }
+
+            // push to the back
+            voting.offer(seat);
+        }
+
+        return "";
+    }
 }
--- queue/src/test/java/QueueTest.java
+++ queue/src/test/java/QueueTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -16,5 +17,17 @@ public class QueueTest {
     @Test
     public void recentCounterTest() {
         assertArrayEquals(new int[] { 1, 2, 3, 3 }, sol.recentCounter(new int[] { 1, 100, 3001, 3002 }));
+    }
+
+    @Test
+    public void predictPartyVictory() {
+        assertEquals("Radiant", sol.predictPartyVictory("RD"));
+        assertEquals("Dire", sol.predictPartyVictory("RDD"));
+        assertEquals("Dire", sol.predictPartyVictory("DDRRR"));
+        assertEquals("Radiant", sol.predictPartyVictory("RDDRR"));
+        assertEquals("Dire", sol.predictPartyVictory("RDRDDDRDDRDRDDRRRRRD"));
+        assertEquals("Dire", sol.predictPartyVictory("RDDRRDD"));
+        assertEquals("Radiant", sol.predictPartyVictory("R"));
+        assertEquals("Radiant", sol.predictPartyVictory("RR"));
     }
 }
